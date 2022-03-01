7689f9e0c247f14943faf01a795a4ab4fc3da7e5
David Hilowitz <dhilowitz@gmail.com>
2022-03-01 10:12:48 -0500

    Revert "Commented these out to prevent iOS app store warning"
    This reverts commit d15df2602dbb0e46211df8eadeb22d5fb4a89d3e.

--- modules/juce_gui_basics/native/accessibility/juce_ios_Accessibility.mm
+++ modules/juce_gui_basics/native/accessibility/juce_ios_Accessibility.mm
@@ -219,16 +219,16 @@ private:
             addMethod (@selector (accessibilityDecrement),     accessibilityPerformDecrement);
             addMethod (@selector (accessibilityPerformEscape), accessibilityPerformEscape);
 
-           // #if JUCE_IOS_CONTAINER_API_AVAILABLE
-           //  if (@available (iOS 11.0, *))
-           //  {
-           //      addMethod (@selector (accessibilityDataTableCellElementForRow:column:), getAccessibilityDataTableCellElementForRowColumn);
-           //      addMethod (@selector (accessibilityRowCount),                           getAccessibilityRowCount);
-           //      addMethod (@selector (accessibilityColumnCount),                        getAccessibilityColumnCount);
-           //      addMethod (@selector (accessibilityRowRange),                           getAccessibilityRowIndexRange);
-           //      addMethod (@selector (accessibilityColumnRange),                        getAccessibilityColumnIndexRange);
-           //  }
-           // #endif
+           #if JUCE_IOS_CONTAINER_API_AVAILABLE
+            if (@available (iOS 11.0, *))
+            {
+                addMethod (@selector (accessibilityDataTableCellElementForRow:column:), getAccessibilityDataTableCellElementForRowColumn);
+                addMethod (@selector (accessibilityRowCount),                           getAccessibilityRowCount);
+                addMethod (@selector (accessibilityColumnCount),                        getAccessibilityColumnCount);
+                addMethod (@selector (accessibilityRowRange),                           getAccessibilityRowIndexRange);
+                addMethod (@selector (accessibilityColumnRange),                        getAccessibilityColumnIndexRange);
+            }
+           #endif
 
             if (elementType == Type::textElement)
             {
